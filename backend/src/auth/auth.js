86fac2f7b940cf417963e9fc8bd962336c866867
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-12-13 17:35:11 -0300

    Criando login e autenticação na rota /login. Corrigido erros no código

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -5,14 +5,17 @@ import crypto from 'crypto';
 import { Mongo } from '../database/mongo.js';
 import jwt from 'jsonwebtoken';
 import { ObjectId } from 'mongodb';
+import { error } from 'console';
+import { text } from 'stream/consumers';
 
 const collectionName = 'users';
+const authRouter = express.Router();
 
 passport.use(
   new LocalStrategy(
     { usernameField: 'email' },
     async (email, password, callback) => {
-      const user = await Mongo.ObjectId.collectionName(collectionName).findOne({
+      const user = await Mongo.db.collection(collectionName).findOne({
         email: email,
       });
 
@@ -20,7 +23,7 @@ passport.use(
         return callback(null, false);
       }
 
-      const saltBuffer = user.salt.saltBuffer;
+      const saltBuffer = user.salt.buffer;
 
       crypto.pbkdf2(
         password,
@@ -47,8 +50,6 @@ passport.use(
     }
   )
 );
-
-const authRouter = express.Router();
 
 authRouter.post('/signup', async (req, res) => {
   const checkUser = await Mongo.db
@@ -110,6 +111,43 @@ authRouter.post('/signup', async (req, res) => {
       }
     }
   );
+});
+
+authRouter.post('/login', (req, res) => {
+  passport.authenticate('local', (error, user) => {
+    if (error) {
+      return res.status(500).send({
+        success: false,
+        statusCode: 500,
+        body: {
+          text: 'Error during authentication',
+          error,
+        },
+      });
+    }
+
+    if (!user) {
+      return res.status(400).send({
+        success: false,
+        statusCode: 400,
+        body: {
+          text: 'User not found',
+        },
+      });
+    }
+
+    const token = jwt.sign(user, 'secret');
+
+    return res.status(200).send({
+      success: true,
+      statusCode: 200,
+      body: {
+        text: 'User logged in correctly',
+        user,
+        token,
+      },
+    });
+  })(req, res);
 });
 
 export default authRouter;
