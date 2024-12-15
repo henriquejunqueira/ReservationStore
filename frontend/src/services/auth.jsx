41d13faeef204acd03f596fc841badddab56283d
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-12-14 22:44:42 -0300

    Criada autenticação utilizando JWT na página de login

---
+++ frontend/src/services/auth.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+export default function AuthServices() {
+  const [authLoading, setAuthLoading] = useState(false);
+
+  const url = 'http://localhost:3000/auth';
+
+  const login = (formData) => {
+    setAuthLoading(true);
+
+    fetch(`${url}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.success && result.body.token) {
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({ token: result.body.token, user: result.body.user })
+          );
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setAuthLoading(false);
+      });
+  };
+
+  const signup = (formData) => {
+    setAuthLoading(true);
+
+    fetch(`${url}/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success && result.body.token) {
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({ token: result.body.token, user: result.body.user })
+          );
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setAuthLoading(false);
+      });
+  };
+
+  const logout = () => {};
+
+  return { signup, login, logout, authLoading };
+}
