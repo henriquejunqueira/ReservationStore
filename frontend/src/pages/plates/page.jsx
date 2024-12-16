5c001a144f515971f78f3c8336c35df5e0b010cf
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-12-16 16:06:51 -0300

    Consumindo API Rest para comunicar via GET o serviço de pratos com o banco de dados

--- frontend/src/pages/plates/page.jsx
+++ frontend/src/pages/plates/page.jsx
@@ -1,3 +1,22 @@
+import { useEffect } from 'react';
+import PlateServices from '../../services/plates';
+import Loading from '../loading/page';
+
 export default function Plates() {
+  const { getAvailablePlates, platesList, platesLoading, refetchPlates } =
+    PlateServices();
+
+  useEffect(() => {
+    if (refetchPlates) {
+      getAvailablePlates();
+    }
+  }, [refetchPlates]);
+
+  if (platesLoading) {
+    return <Loading />;
+  }
+
+  console.log(platesList);
+
   return <h1>Plates</h1>;
 }
