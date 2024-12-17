ebbd5fc47e02ff6b4df78c3a087d632bc00989b2
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-12-17 11:25:56 -0300

    Estilizando o carrinho. Adicionado botões de quantidade de itens, remover item e confirmar pedido no carrinho

--- frontend/src/pages/cart/page.jsx
+++ frontend/src/pages/cart/page.jsx
@@ -1,9 +1,54 @@
 import { useCartContext } from '../../contexts/useCartContext';
+import styles from './page.module.css';
+import { BsCartDash } from 'react-icons/bs';
+import { BiLike } from 'react-icons/bi';
 
 export default function Cart() {
   const { cartItems } = useCartContext();
 
-  console.log(cartItems);
+  if (!cartItems.length) {
+    return (
+      <div>
+        <h1>Your cart is empty...</h1>
+        <button>See our specialities</button>
+      </div>
+    );
+  }
 
-  return <h1>Cart</h1>;
+  return (
+    <div className={styles.pageContainer}>
+      <h1>Your items:</h1>
+      <section>
+        <div className={styles.itemsListContainer}>
+          {cartItems.map((item) => (
+            <div className={styles.itemContainer} key={item._id}>
+              <img src={item.imgUrl} alt='' />
+              <div className={styles.itemContent}>
+                <h2>{item.name}</h2>
+                <p>[{String(item.ingredients)}]</p>
+                <p>{item.description}</p>
+                <div className={styles.portionContainer}>
+                  <p>Portions:</p>
+                  <p>{item.quantity}</p>
+                  <div className={styles.portionBtns}>
+                    <button>-</button>
+                    <button>+</button>
+                  </div>
+                </div>
+                <button>
+                  <BsCartDash /> Remove item
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <div className={styles.containerBtn}>
+        <button className={styles.confirmBtn}>
+          <BiLike />
+          Confirm your order!
+        </button>
+      </div>
+    </div>
+  );
 }
