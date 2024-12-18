67ecb9388678e5efa05264523f6d88dfe26894b1
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-12-18 11:55:44 -0300

    Criado o popUp de confirmação do pedido. Feito método post para salvar o pedido no banco de dados. Feita estilização do popUp de confirmação

--- frontend/src/pages/cart/page.jsx
+++ frontend/src/pages/cart/page.jsx
@@ -2,9 +2,15 @@ import { useCartContext } from '../../contexts/useCartContext';
 import styles from './page.module.css';
 import { BsCartDash } from 'react-icons/bs';
 import { BiLike } from 'react-icons/bi';
+import { useState } from 'react';
+import ConfirmOrderPopup from '../../components/confirmOrderPopup/confirmOrderPopup';
+import OrderServices from '../../services/order';
 
 export default function Cart() {
-  const { cartItems, updateCartItems, removeFromCart } = useCartContext();
+  const { cartItems, updateCartItems, removeFromCart, clearCart } =
+    useCartContext();
+  const [confirmPopupOpen, setConfirmPopupOpen] = useState(false);
+  const { sendOrder } = OrderServices();
 
   const handleChangeItemQty = (mode, itemId) => {
     const updatedCartItem = cartItems.map((item) => {
@@ -22,6 +28,20 @@ export default function Cart() {
     updateCartItems(updatedCartItem);
   };
 
+  const handleOpenPopup = (e) => {
+    e.preventDefault();
+    setConfirmPopupOpen(!confirmPopupOpen);
+  };
+
+  const handleConfirmOrder = (orderData) => {
+    orderData.items = cartItems.map((item) => {
+      return { plateId: item._id, quantity: item.quantity };
+    });
+    sendOrder(orderData);
+    setConfirmPopupOpen(!confirmPopupOpen);
+    clearCart();
+  };
+
   if (!cartItems.length) {
     return (
       <div>
@@ -32,56 +52,64 @@ export default function Cart() {
   }
 
   return (
-    <div className={styles.pageContainer}>
-      <h1>Your items:</h1>
-      <section>
-        <div className={styles.itemsListContainer}>
-          {cartItems.map((item) => (
-            <div className={styles.itemContainer} key={item._id}>
-              <img src={item.imgUrl} alt='' />
-              <div className={styles.itemContent}>
-                <h2>{item.name}</h2>
-                <p>[{String(item.ingredients)}]</p>
-                <p>{item.description}</p>
-                <div className={styles.portionContainer}>
-                  <p>Portions:</p>
-                  <p>{item.quantity}</p>
-                  <div className={styles.portionBtns}>
-                    <button
-                      onClick={() => {
-                        handleChangeItemQty('less', item._id);
-                      }}
-                    >
-                      -
-                    </button>
-                    <button
-                      onClick={() => {
-                        handleChangeItemQty('more', item._id);
-                      }}
-                    >
-                      +
-                    </button>
+    <>
+      <div className={styles.pageContainer}>
+        <h1>Your items:</h1>
+        <section>
+          <div className={styles.itemsListContainer}>
+            {cartItems.map((item) => (
+              <div className={styles.itemContainer} key={item._id}>
+                <img src={item.imgUrl} alt='' />
+                <div className={styles.itemContent}>
+                  <h2>{item.name}</h2>
+                  <p>[{String(item.ingredients)}]</p>
+                  <p>{item.description}</p>
+                  <div className={styles.portionContainer}>
+                    <p>Portions:</p>
+                    <p>{item.quantity}</p>
+                    <div className={styles.portionBtns}>
+                      <button
+                        onClick={() => {
+                          handleChangeItemQty('less', item._id);
+                        }}
+                      >
+                        -
+                      </button>
+                      <button
+                        onClick={() => {
+                          handleChangeItemQty('more', item._id);
+                        }}
+                      >
+                        +
+                      </button>
+                    </div>
                   </div>
+                  <button
+                    onClick={() => {
+                      removeFromCart(item._id);
+                    }}
+                  >
+                    <BsCartDash />
+                    Remove item
+                  </button>
                 </div>
-                <button
-                  onClick={() => {
-                    removeFromCart(item._id);
-                  }}
-                >
-                  <BsCartDash />
-                  Remove item
-                </button>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        </section>
+        <div className={styles.containerBtn}>
+          <button className={styles.confirmBtn} onClick={handleOpenPopup}>
+            <BiLike />
+            Confirm your order!
+          </button>
         </div>
-      </section>
-      <div className={styles.containerBtn}>
-        <button className={styles.confirmBtn}>
-          <BiLike />
-          Confirm your order!
-        </button>
       </div>
-    </div>
+
+      <ConfirmOrderPopup
+        open={confirmPopupOpen}
+        onClose={handleOpenPopup}
+        onConfirm={handleConfirmOrder}
+      />
+    </>
   );
 }
--- frontend/src/services/order.jsx
+++ frontend/src/services/order.jsx
@@ -34,5 +34,29 @@ export default function OrderServices() {
       });
   };
 
-  return { getUserOrders, orderLoading, refetchOrders, ordersList };
+  const sendOrder = (orderData) => {
+    setOrderLoading(true);
+
+    fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(orderData),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setOrderLoading(false);
+        setRefetchOrders(false);
+      });
+  };
+
+  return { getUserOrders, orderLoading, refetchOrders, ordersList, sendOrder };
 }
